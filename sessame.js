1388fd64d676840939d22117c87661bdd056e7e4
TuckerH <tucker.humiston.nexus@gmail.com>
2023-04-13 08:06:23 -0400

    Wroking nav

--- sessame.js
+++ sessame.js
@@ -4,37 +4,46 @@ let isOpen = false;
 //Open Sessame Nav Bar
 let sessameButton = document.getElementById('openSessame');
 let navBar = document.getElementById('nav');
+
+
 const openSessame = function() {
     
+    //Open
     if (!isOpen) {
         //Variables
         let contact = document.getElementById('contact');
         let projects = document.getElementById('projects');
-        let about = document.getElementById('about')
-
+        let about = document.getElementById('about');
+        let home = document.getElementById('home');
         //Open Nav Bar
         navBar.style.width = '300px';
         document.getElementById('drop').style.width = '300px';
         document.body.style.gridTemplateColumns = "300px 1fr 300px";
         document.getElementById('openSessame').innerHTML = '>>>';
-        document.getElementById('location').style.display = 'none';
-
-        //Add Page links
+        //Rotate page links
+        home.style.writingMode = 'horizontal-tb';
         contact.style.writingMode = 'horizontal-tb';
         projects.style.writingMode = 'horizontal-tb';
         about.style.writingMode = 'horizontal-tb';
+        //Add and space page links
+        home.innerHTML = "<a href='./index.html'>Home</a>";
         projects.innerHTML = "<a href='./projects.html'>Projects</a>";
         about.innerHTML = "<a href='./about.html'>About Me</a>";
         contact.innerHTML = "<a href='./contact.html'>Contact Me</a>";
-        about.style.marginTop = "1em"
-        contact.style.marginTop = "1em"
+        home.style.marginTop = '6.5em';
+        about.style.marginTop = "1em";
+        contact.style.marginTop = "1em";
+        projects.style.marginTop = "1em";
+    //Close
     } else {
         navBar.style.width = '';
         document.getElementById('drop').style.width = '';
         document.body.style.gridTemplateColumns = "";
         document.getElementById('openSessame').innerHTML = '<<<';
-        document.getElementById('location').style.display = '';
+        home.style.display = '';
+        home.style.writingMode = "vertical-rl";
 
+        home.innerHTML = '';
         projects.innerHTML = '';
         about.innerHTML = '';
         contact.innerHTML = '';
